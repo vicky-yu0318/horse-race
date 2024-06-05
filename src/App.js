5e8d02096711dabf706b123f9ca7503a6920400b
vicky-yu <freedom0318v@gmail.com>
2024-06-06 02:22:57 +0800

    feat: add manual baseSpeed adjustment function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 import _ from "lodash";
 import horse from "./images/horse.png";
+import { useForm } from "react-hook-form";
 
 const horseCount = 7;
-const baseSpeed = 200; // ms per frame
+// const baseSpeed = 200; // ms per frame
 const trackLength = window.innerWidth - 40;
 
 function App() {
@@ -12,6 +13,15 @@ function App() {
   const [bet, setBet] = useState(null);
   const [winner, setWinner] = useState(null);
   const intervalRef = useRef(null);
+  const [baseSpeed, setBaseSpeed] = useState(200);
+  const {
+    register,
+    handleSubmit,
+    trigger,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: "onChange",
+  });
 
   useEffect(() => {
     if (positions.every((pos) => pos >= trackLength)) {
@@ -58,9 +68,53 @@ function App() {
     }
   };
 
+  const onSubmit = (data) => {
+    setBaseSpeed(data.baseSpeed);
+  };
+  const handleChange = async (e) => {
+    await trigger("baseSpeed");
+  };
+
   return (
     <div className="App">
-      <h1>Horse Race</h1>
+      <h1 style={{ textAlign: "center" }}>Horse Race</h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div>
+          <label>Base Speed:</label>
+
+          <div>
+            <input
+              onChange={handleChange}
+              type="text"
+              defaultValue={baseSpeed}
+              {...register("baseSpeed", {
+                required: "必填欄位",
+                pattern: {
+                  value: /^\d+$/,
+                  message: "只能輸入數字",
+                },
+              })}
+            />
+            <input
+              type="submit"
+              disabled={!isValid}
+              style={{ marginLeft: 5 }}
+            />
+          </div>
+          {errors.baseSpeed && (
+            <div>
+              {<span style={{ color: "red" }}>{errors.baseSpeed.message}</span>}
+            </div>
+          )}
+          {!errors.baseSpeed && <div style={{ padding: "11px 0" }}></div>}
+        </div>
+      </form>
       <div className="track">
         {positions.map((position, index) => (
           <div
@@ -76,17 +130,18 @@ function App() {
           </div>
         ))}
       </div>
-      {_.isNumber(bet) && (
-        <button onClick={startRace} disabled={winner !== null}>
-          Start Race
-        </button>
-      )}
-
-      {_.isNumber(bet) && _.isNumber(winner) && (
-        <button onClick={restartRace}>Restart Race</button>
-      )}
-      {!_.isNumber(bet) && <h2>請點選馬匹下注哪批馬兒跑最快</h2>}
-      {_.isNumber(bet) && <div>你賭注{bet + 1}號小馬跑最快</div>}
+      <div style={{ textAlign: "center" }}>
+        {_.isNumber(bet) && (
+          <button onClick={startRace} disabled={winner !== null}>
+            Start Race
+          </button>
+        )}
+        {_.isNumber(bet) && _.isNumber(winner) && (
+          <button onClick={restartRace}>Restart Race</button>
+        )}
+        {!_.isNumber(bet) && <h3>請點選馬匹下注哪批馬兒跑最快</h3>}
+        {_.isNumber(bet) && <div>你賭注{bet + 1}號小馬跑最快</div>}
+      </div>
     </div>
   );
 }
